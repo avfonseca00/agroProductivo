8893d6a19fa419976fca2647b26d027059f87edd
avfonseca00 <90794391+avfonseca00@users.noreply.github.com>
2023-01-18 20:07:16 -0500

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+const About = () => {
+  return (
+    <div>
+      <h1> Hola Mundo! </h1>
+      <Button>Hola</Button>
+    </div>
+  );
+};
+
+export default About;
